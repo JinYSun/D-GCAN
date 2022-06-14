e4bb8b30d854de9367e312268d903cdcf2aba1b2
JinyuSun <jinyusun@csu.edu.cn>
2022-06-14 22:06:28 +0800

    Update run.py

--- D_GCAN/run.py
+++ D_GCAN/run.py
@@ -8,17 +8,8 @@ import torch
 import pandas as pd
 import train
 import predict
-test = train.train('../dataset/world_wide.txt')
-f =pd.read_table('../dataset/zinc15.txt')
+train = train.train('../dataset/world_wide.txt')
+
 #predict = predict.predict('../dataset/world_wide.txt',property=True)
-torch.cuda.empty_cache()
-for i in range (307):
-    file = f.iloc[1000*i:1000*i+1000,0]
-    file=pd.DataFrame(file)
-    file.to_csv('../dataset/zinc15/'+str(i)+'.txt',index=None)
-    
-for i in range (307):
-    predict = predict.predict('../dataset/zinc15/'+str(i)+'.txt',property=False)
-    res=pd.DataFrame(predict)
-    res.to_csv('../dataset/zinc15/'+str(i)+'.csv',index=None)
+
  
